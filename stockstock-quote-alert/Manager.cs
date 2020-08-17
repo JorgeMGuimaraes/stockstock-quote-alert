da3bb698129e87aa6e6434098cffdfd77826470b
Jorge Marinho Guimaraes <jmguimaraes@id.uff.br>
2020-08-17 18:20:25 -0300

    Criado

---
+++ stockstock-quote-alert/Manager.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stockstock_quote_alert {
+    class Manager {
+        public Manager() => Console.WriteLine("Hello");
+    }
+}
