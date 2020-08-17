0dd1fc4d64a07758b5637b23e05863067d789bf5
Jorge Marinho Guimaraes <jmguimaraes@id.uff.br>
2020-08-17 18:09:23 -0300

    First commit

---
+++ stockstock-quote-alert/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stockstock_quote_alert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
