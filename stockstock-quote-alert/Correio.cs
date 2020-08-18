15817d776087547e78943b5ce17e93b89322ff9e
Jorge Marinho Guimaraes <jmguimaraes@id.uff.br>
2020-08-17 22:43:36 -0300

    criado;

---
+++ stockstock-quote-alert/Correio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Mail;
+
+namespace stockstock_quote_alert {
+    internal class Correio {
+        #region Campos
+        private MailMessage Mail { get; set; }
+        #endregion
+        #region Constructor
+        internal Correio() {
+            Mail = new MailMessage();
+        }
+        #endregion
+    }
+}
