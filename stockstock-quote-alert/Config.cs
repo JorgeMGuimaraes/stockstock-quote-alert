f41e5ce44a7b50bf45c95dfe6e4075a37a5b951a
Jorge Guimaraes <jorgesoftwaredev@outlook.com>
2020-08-19 03:26:29 -0300

    arquivo de configuracoes criado

---
+++ stockstock-quote-alert/Config.cs
@@ -0,0 +1,16 @@
+﻿namespace stockstock_quote_alert {
+    /// <summary>
+    /// Configuracoes basicas copiadas do arquivo configs.txt em disco.
+    /// </summary>
+    internal class Config {
+        public string MailDe { get; set; }
+        public string NomeDe { get; set; }
+        public string MailPara { get; set; }
+        public string NomePara { get; set; }
+        public string MailSenha { get; set; }
+        public string Assunto { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string UrlApi { get; set; }
+    }
+}
