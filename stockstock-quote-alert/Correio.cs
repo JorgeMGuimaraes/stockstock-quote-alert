7b6c4ec7b11b76fc9c8040c2d087c3515e0e7463
Jorge Guimaraes <jorgesoftwaredev@outlook.com>
2020-08-19 04:54:27 -0300

    leitura e config movidas para classes proprias

--- stockstock-quote-alert/Correio.cs
+++ stockstock-quote-alert/Correio.cs
@@ -11,105 +11,51 @@ namespace stockstock_quote_alert {
     /// </summary>
     internal class Correio {
         #region Campos
-        private MimeMessage mensagem { get; set; }
-        private readonly Config confMail;
-        private string CaminhoArquivo { get; } = "configs.txt";
-        /// <summary>
-        /// Configuracoes basicas copiadas do arquivo configs.txt em disco.
-        /// </summary>
-        private class Config {
-            public string MailDe { get; set; }
-            public string NomeDe { get; set; }
-            public string MailPara { get; set; }
-            public string NomePara { get; set; }
-            public string MailSenha { get; set; }
-            public string Assunto { get; set; }
-            public string Host { get; set; }
-            public int Port { get; set; }
-        }
+        private Config ConfigMail { get; }
         #endregion
         #region Constructor
-        internal Correio() {
-            confMail = LerConfigDeArquivo(CaminhoArquivo);
-            if (ConfigVazia()) {
-                Console.WriteLine("Favor preencher arquivo configs.txt com seus dados");
-            }
-        }
-
+        internal Correio(Config config) => ConfigMail = config;
         #endregion
         #region Metodos
         /// <summary>
         /// Gera um objeto de mensagem baseado nas configuracoes do usuario.
-        /// TODO: Pode-se implementar um template de corpo de email vindo de um arquivo.
+        /// TODO: Pode-se implementar um template de corpo de email lido de um arquivo.
         /// </summary>
-        /// <param name="stock">nome da stock acompanhada.</param>
+        /// <param name="stock">Stock acompanhada.</param>
         /// <param name="tipo">tipo da operacao: compra ou venda</param>
-        /// <param name="valor">valor fixado da operacao</param>
         /// <returns>Mensagem pronta para ser enviada</returns>
-        private MimeMessage Mensagem(string stock, string tipo, double valor) {
+        private MimeMessage Mensagem(Stock stock, string tipo) {
             var mail        = new MimeMessage();
-            mail.From.Add(new MailboxAddress(confMail.NomeDe, confMail.MailDe));
-            mail.To.Add(new MailboxAddress(confMail.NomePara, confMail.MailPara));
-            mail.Subject    = $"{confMail.Assunto}: {stock} atingiu valor de {tipo}";
+            mail.From.Add(new MailboxAddress(ConfigMail.NomeDe, ConfigMail.MailDe));
+            mail.To.Add(new MailboxAddress(ConfigMail.NomePara, ConfigMail.MailPara));
+            mail.Subject    = $"{ConfigMail.Assunto}: {stock} atingiu valor de {tipo}";
             mail.Body       = new TextPart("plain") {
-                Text        = $@"Sr. {confMail.NomePara},
-    Conforme suas configurações de compra e venda a stock {stock} atingiu o preço de {tipo}, de {valor}.
-
-{confMail.NomeDe}"
-            };
+                Text        = $@"   Sr. {ConfigMail.NomePara},
+Conforme suas configurações de compra e venda a stock {stock.Simbolo} atingiu o preço de {tipo}, de {stock.PrecoAtual}.
+Dados Gerais:
+    Stock: {stock.Simbolo} - {stock.Nome}
+    Preço de {tipo}: R$ {((tipo == "venda") ? stock.PrecoVenda : stock.PrecoCompra)}
+    Preço Atual: R$ {stock.PrecoAtual}
+    Atualizado em: {stock.UltimaAtualizacao}"
+                };
             return mail;
         }
         /// <summary>
         /// Usa os parametros de mensagem para enviar um email ao usuario
-        /// TODO: Pode-se generalizar o metodo afim de enviar mesagens para tipos diferentes de servidores, ex.: EnviarMensagem<TipoServidor>.
+        /// TODO: Pode-se generalizar o metodo afim de enviar mesagens para tipos diferentes de servidores, ex.: EnviarMensagem<ITipoServidor>.
         /// </summary>
         /// <param name="stock"></param>
         /// <param name="tipo"></param>
-        /// <param name="valor"></param>
-        public void EnviarMensagem(string stock, string tipo, double valor) {
+        public void EnviarMensagem(Stock stock, string tipo) {
+            Console.WriteLine($"Enviando nova mensagem: {stock.Simbolo} -> {tipo}");
             using (var cliente = new SmtpClient()) {
-                cliente.Connect(confMail.Host, confMail.Port, false);
-                cliente.Authenticate(confMail.MailDe, confMail.MailSenha);
-                cliente.Send(Mensagem(stock, tipo, valor));
+                cliente.Connect(ConfigMail.Host, ConfigMail.Port, false);
+                cliente.Authenticate(ConfigMail.MailDe, ConfigMail.MailSenha);
+                cliente.Send(Mensagem(stock, tipo));
                 cliente.Disconnect(true);
             }
+            Console.WriteLine($"Mensagem enviada.");
             return;
-        }
-        /// <summary>
-        /// Le um arquivo de configuracoes salvo em disco e converte num objeto de configuracoes
-        /// </summary>
-        /// <returns>Configuracoes gerais de envio de email</returns>
-        private Config LerConfigDeArquivo(string nomeArquivo) {
-            var configsLidas = new Dictionary<string, string>();
-            foreach (var line in File.ReadAllLines(nomeArquivo)) {
-                var itens = line.Split('=');
-                configsLidas[itens[0]] = itens[1];
-            }
-            return new Config {
-                MailDe              = configsLidas["MailDe"],
-                NomeDe              = configsLidas["NomeDe"],
-                MailSenha           = configsLidas["MailSenha"],
-                MailPara            = configsLidas["MailPara"],
-                NomePara            = configsLidas["NomePara"],
-                Assunto             = configsLidas["Assunto"],
-                Host                = configsLidas["Host"],
-                Port                = int.Parse(configsLidas["Port"])
-            };
-        }
-        /// <summary>
-        /// Confere se algum campo do arquivo de configuracao esta em branco.
-        /// </summary>
-        /// <returns>True, se esta totalmente preenchido.</returns>
-        private bool ConfigVazia() {
-            return
-                confMail.MailDe     == string.Empty ||
-                confMail.NomeDe     == string.Empty ||
-                confMail.MailPara   == string.Empty ||
-                confMail.NomePara   == string.Empty ||
-                confMail.MailSenha  == string.Empty ||
-                confMail.Assunto    == string.Empty ||
-                confMail.Host       == string.Empty ||
-                confMail.Port       == 0;
         }
         #endregion
     }
---
+++ stockstock-quote-alert/IOConfiguracoes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace stockstock_quote_alert {
+    internal class IOConfiguracoes {
+        #region Campos
+        #endregion
+        #region Metodos
+        /// <summary>
+        /// Le um arquivo de configuracoes salvo em disco e converte num objeto de configuracoes
+        /// </summary>
+        /// <returns>Configuracoes gerais de envio de email</returns>
+        internal static Config GetConfig(string nomeArquivo) {
+                        var configsLidas = new Dictionary<string, string>();
+            foreach (var line in File.ReadAllLines(nomeArquivo)) {
+                var itens = line.Split('=');
+                configsLidas[itens[0]] = itens[1];
+            }
+            var config = new Config {
+                MailDe              = configsLidas["MailDe"],
+                NomeDe              = configsLidas["NomeDe"],
+                MailSenha           = configsLidas["MailSenha"],
+                MailPara            = configsLidas["MailPara"],
+                NomePara            = configsLidas["NomePara"],
+                Assunto             = configsLidas["Assunto"],
+                Host                = configsLidas["Host"],
+                Port                = int.Parse(configsLidas["Port"]),
+                UrlApi              = configsLidas["UrlApi"],
+                ApiKey              = configsLidas["ApiKey"]
+            };
+            return (ConfigValida(config)) ? config : null;
+        }
+        /// <summary>
+        /// Confere se algum campo do arquivo de configuracao esta em branco.
+        /// </summary>
+        /// <returns>True, se esta totalmente preenchido.</returns>
+        private static bool ConfigValida(Config ConfigMail) {
+            return
+                ConfigMail.MailDe       != string.Empty &&
+                ConfigMail.NomeDe       != string.Empty &&
+                ConfigMail.MailPara     != string.Empty &&
+                ConfigMail.NomePara     != string.Empty &&
+                ConfigMail.MailSenha    != string.Empty &&
+                ConfigMail.Assunto      != string.Empty &&
+                ConfigMail.Host         != string.Empty &&
+                ConfigMail.Port         != 0;
+        }
+    }
+    #endregion
+}
